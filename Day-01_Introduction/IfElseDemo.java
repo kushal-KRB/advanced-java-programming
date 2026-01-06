896ff5cdadcf179c77df697f81e2604d96a49fd5
ranabhatkushal01@gmail.com <ranabhatkushal01@gmail.com>
2026-01-06 20:14:40 +0545

    Day 1: Added IfElseDemo, OperatorsDemo, and javaBasics programs

---
+++ Day-01_Introduction/IfElseDemo.java
@@ -0,0 +1,13 @@
+public class IfElseDemo {
+
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 40) {
+            System.out.println("Result: Pass");
+        } else {
+            System.out.println("Result: Fail");
+        }
+    }
+}
---
+++ Day-01_Introduction/javaBasics.java
@@ -0,0 +1,18 @@
+public class JavaBasics {
+
+    public static void main(String[] args) {
+
+        // Printing output
+        System.out.println("Advanced Java Programming");
+        System.out.println("BSc CSIT - 7th Semester");
+
+        // Variables
+        int semester = 7;
+        String course = "Advanced Java Programming";
+        double version = 17.0;
+
+        System.out.println("Semester: " + semester);
+        System.out.println("Course: " + course);
+        System.out.println("Java Version: " + version);
+    }
+}
